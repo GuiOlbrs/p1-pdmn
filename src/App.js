190e6b8718d53059c1a8b22f222fdf836e4529d3
Guilherme Oliveira <guilherme.olbrs34@hotmail.com>
2024-10-08 23:07:05 -0300

    feat(p1-pdmn): exibe lista ficticia

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import TarefaEntrada from './TarefaEntrada'
+import TarefaLista from './TarefaLista'
 
 
 const App = () => {
     return (
             <div className='container mt-4'>
-                 <TarefaEntrada></TarefaEntrada>
                 <div className='row'>
-                    <div className='col-sm-12 col-md-10 mx-auto'>
-                        <div className="card">
-                            <h1 className='display-5 text-center'>
-                                Hello, tarefas
-                            </h1>
-                        </div>
-                    </div>
+                    <TarefaEntrada></TarefaEntrada>
+                </div>
+                <div className='row'>
+                    <TarefaLista></TarefaLista>
                 </div>
             </div>   
         )
---
+++ src/TarefaLista.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const TarefaLista = () => {
+    return (
+        <div className='container'>
+            <div className='col-sm-12 col-md-10 mx-auto border-black' style={{backgroundColor: '#FAFAD2'}}>
+                <div className='row text-center'>
+                    <div className='bg-white border p-4 mx-auto my-3 border-black rounded col-8'>Preparar aula de programação</div>
+                    <div className='bg-white border p-4 mx-auto my-3 border-black rounded col-8'>Fazer feira</div>
+                    <div className='bg-white border p-4 mx-auto my-3 border-black rounded col-8'>Preparar marmitas</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default TarefaLista
